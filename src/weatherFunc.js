2fbef7af1f5986e1920d1eca2e85808596c14780
RomaSub <antonfrolov1212@gmail.com>
2023-06-06 14:59:58 +0300

    объединил два файла, теперь api работает с введеными данными

--- src/weatherFunc.js
+++
@@ -1,9 +0,0 @@
-const fetchData = async () => {
-  const link = 'http://api.weatherstack.com/current?access_key=3ec035a6c71364180f51f129f7014668&query=Moscow';
-
-  const result = await fetch(link);
-  const data = await result.json();
-
-  return data;
-};
-export default fetchData;

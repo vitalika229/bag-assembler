c4cbfa63afc576b7b171bba548052644851a9822
bitsen03 <bitsen03@mail.ru>
2023-06-06 11:40:11 +0300

    npm lodash add routeInfo

---
+++ src/routeInformation.js
@@ -0,0 +1,16 @@
+import * as readline from 'node:readline/promises';
+import { stdin as input, stdout as output } from 'node:process';
+import _ from 'lodash';
+
+const routeInformation = async () => {
+  const rl = readline.createInterface({ input, output });
+  const answerStartCity = await rl.question('Откуда вы летите? ');
+  const answerEndCity = await rl.question('Куда вы летите? ');
+  const capitalizeCities = [_.capitalize(answerStartCity), _.capitalize(answerEndCity)];
+  console.log(`${capitalizeCities[0]} => ${capitalizeCities[1]}`);
+
+  rl.close();
+  return capitalizeCities;
+};
+
+routeInformation();

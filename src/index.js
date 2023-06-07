75c740e5e108d43d8f12d307593c1a4f3aa05142
Sergei Kiselev <kiselev.s.d.98@gmail.com>
2023-06-07 10:11:28 +0300

    add dir for tests; install jest

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-import wf from './writeFile.js';
+import writeFile from './writeFile.js';
 import reg from './nameRegistration.js';
 import routeInformation from './routeInformation.js';
 
 export default async () => {
   const data = await reg();
-  wf('./users/user.json', data);
+  writeFile('./users/user.json', data);
   const temperature = await routeInformation();
   console.log(temperature);
 };

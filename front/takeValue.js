6ae18ba5fc3352638b26d12b7a7a1373e8f32808
Sergei Kiselev <kiselev.s.d.98@gmail.com>
2023-06-07 12:40:34 +0300

    fix lint

--- front/takeValue.js
+++ front/takeValue.js
@@ -1,6 +1,6 @@
-const input = document.querySelector('button').onclick = myClick;
+// const input = document.querySelector('button').onclick = myClick;
 
-function myClick() {
-  const name = document.querySelector('input').value;
-  console.log(name);
-}
+// function myClick() {
+//   const name = document.querySelector('input').value;
+//   console.log(name);
+// }
